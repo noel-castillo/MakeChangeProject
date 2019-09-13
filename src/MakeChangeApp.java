ee1fa9db2073a8d912f11720f290fdeb5dadca06
Noel Castillo <noelcastillo@Administrators-MacBook-Pro.local>
2019-09-13 17:35:44 -0600

    Start README overview

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -6,10 +6,16 @@ public class MakeChangeApp {
 
 		Scanner keyboard = new Scanner(System.in);
 		double priceOfItem = priceOfItem(keyboard);
+		
 	}
 
 	public static double priceOfItem(Scanner keyboard) {
 		System.out.print("Enter price of item: ");
+		double output = keyboard.nextDouble();
+		return output;
+	}
+	public static double amountPaid(Scanner keyboard) {
+		System.out.print("Enter amount paid to cashier: ");
 		double output = keyboard.nextDouble();
 		return output;
 	}
