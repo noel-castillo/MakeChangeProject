2d36aadf6ae36ab99d901e87811ec666c9bd8d35
Noel Castillo <noelcastillo@Administrators-MacBook-Pro.local>
2019-09-13 17:26:20 -0600

    Create README.md

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
 
 		Scanner keyboard = new Scanner(System.in);
-		System.out.print("Enter price of item: ");
+		double priceOfItem = priceOfItem(keyboard);
 	}
 
+	public static double priceOfItem(Scanner keyboard) {
+		System.out.print("Enter price of item: ");
+		double output = keyboard.nextDouble();
+		return output;
+	}
 }
