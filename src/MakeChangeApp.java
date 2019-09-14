2b0d6a1d246083d44dbee89e67e7711b29806e49
Noel Castillo <noelcastillo@Administrators-MacBook-Pro.local>
2019-09-13 18:48:35 -0600

    Adjusted for when no change is due.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -42,55 +42,59 @@ public class MakeChangeApp {
 
 	public static double changeDue(double amountPaid, double priceOfItem) {
 		double output = amountPaid - priceOfItem, twenties, tens, fives, ones, quarters, dimes, nickels, pennies;
-		System.out.println(output);
-		if (output / 20 >= 1) {
-			twenties = (int) (output / 20);
-			output = output - (twenties * 20);
-			System.out.print((int) twenties + " twenties, ");
-			System.out.println(output);
-		}
-		if (output / 10 >= 1) {
-			tens = (int) (output / 10);
-			output = output - (tens * 10);
-			System.out.print((int) tens + " tens, ");
-			System.out.println(output);
-		}
-		if (output / 5 >= 1) {
-			fives = (int) (output / 5);
-			output = output - (fives * 5);
-			System.out.print((int) fives + " fives, ");
-			System.out.println(output);
-		}
-		if (output / 1 >= 1) {
-			ones = (int) (output / 1);
-			output = output - (ones);
-			System.out.print((int) ones + " ones, ");
-			System.out.println(output);
-		}
-		if (output / .25 >= 1) {
-			quarters = (int) (output / .25);
-			output = output - (quarters * .25);
-			System.out.print((int) quarters + " quarters, ");
-			System.out.println(output);
-		}
-		if (output / .10 >= 1) {
-			dimes = (int) (output / .10);
-			output = output - (dimes * .10);
-			System.out.print((int) dimes + " dimes, ");
-			System.out.println(output);
-		}
-		if (output / .05 >= 1) {
-			nickels = (int) (output / .05);
-			output = output - (nickels * .05);
-			System.out.print((int) nickels + " nickels,");
-			System.out.println(output);
-		}
-		if (output >= 0) {
-			pennies = (int) ((output + 0.01) * 100);
-			System.out.print(" and " + pennies + " pennies.");
-			System.out.println(output);
-		}
 
+		if (amountPaid == priceOfItem) {
+			System.out.println("No change is due");
+		} else {
+			System.out.print("Change due is: ");
+			if (output / 20 >= 1) {
+				twenties = (int) (output / 20);
+				output = output - (twenties * 20);
+				System.out.print((int) twenties + " twenties, ");
+				System.out.println(output);
+			}
+			if (output / 10 >= 1) {
+				tens = (int) (output / 10);
+				output = output - (tens * 10);
+				System.out.print((int) tens + " tens, ");
+				System.out.println(output);
+			}
+			if (output / 5 >= 1) {
+				fives = (int) (output / 5);
+				output = output - (fives * 5);
+				System.out.print((int) fives + " fives, ");
+				System.out.println(output);
+			}
+			if (output / 1 >= 1) {
+				ones = (int) (output / 1);
+				output = output - (ones);
+				System.out.print((int) ones + " ones, ");
+				System.out.println(output);
+			}
+			if (output / .25 >= 1) {
+				quarters = (int) (output / .25);
+				output = output - (quarters * .25);
+				System.out.print((int) quarters + " quarters, ");
+				System.out.println(output);
+			}
+			if (output / .10 >= 1) {
+				dimes = (int) (output / .10);
+				output = output - (dimes * .10);
+				System.out.print((int) dimes + " dimes, ");
+				System.out.println(output);
+			}
+			if (output / .05 >= 1) {
+				nickels = (int) (output / .05);
+				output = output - (nickels * .05);
+				System.out.print((int) nickels + " nickels,");
+				System.out.println(output);
+			}
+			if ((output * 100) >= 1) {
+				pennies = (int) ((output + 0.01) * 100);
+				System.out.print(" and " + pennies + " pennies.");
+				System.out.println(output);
+			}
+		}
 		return output;
 	}
 }
