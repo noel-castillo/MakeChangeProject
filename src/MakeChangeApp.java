7a84e94258d58c2b682522cf1245788d9b7fc71c
Noel Castillo <noelcastillo@Administrators-MacBook-Pro.local>
2019-09-13 18:34:54 -0600

    Version currently outputs proper numbers

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -6,6 +6,8 @@ public class MakeChangeApp {
 
 		Scanner keyboard = new Scanner(System.in);
 		double priceOfItem = priceOfItem(keyboard);
+		double amountPaid = amountPaid(keyboard, priceOfItem);
+		System.out.println(changeDue(amountPaid, priceOfItem));
 		
 	}
 
@@ -13,10 +15,74 @@ public class MakeChangeApp {
 		System.out.print("Enter price of item: ");
 		double output = keyboard.nextDouble();
 		return output;
+		
 	}
-	public static double amountPaid(Scanner keyboard) {
+	public static double amountPaid(Scanner keyboard, double priceOfItem) {
+		String anotherPayment = "";
 		System.out.print("Enter amount paid to cashier: ");
 		double output = keyboard.nextDouble();
+		while(output < priceOfItem) {
+			System.out.println("Amount paid is insufficient."
+					+ "\nWould you like to make an additional payment? (Y/N)");
+			anotherPayment = keyboard.next().toLowerCase();
+			
+			if(anotherPayment.equalsIgnoreCase("y")) {
+				System.out.println("Enter next payment amount: ");
+				output += keyboard.nextDouble();
+			}
+			else if(anotherPayment.equalsIgnoreCase("n")) {
+				break;
+			} else {
+				System.out.println("Invalid response. Try again.");
+			}
+			
+		}
+		return output;
+	}
+	
+	public static double changeDue(double amountPaid, double priceOfItem) {
+		double output = amountPaid - priceOfItem, twenties, tens, 
+				fives, ones, quarters, dimes, nickels, pennies;
+		if(output / 20 >= 1) {
+			twenties = (int) (output / 20);
+			output = output - (twenties * 20);
+			System.out.print((int) twenties + " twenties, ");
+		}
+		if(output / 10 >=1) {
+			tens = (int) (output / 10);
+			output = output - (tens * 20);
+			System.out.print((int) tens + " tens, ");
+		}
+		if(output / 5 >=1) {
+			fives = (int) (output / 5);
+			output = output - (fives * 5);
+			System.out.print((int) fives + " fives, ");
+		}
+		if(output / 1 >=1) {
+			ones = (int) (output / 1);
+			output = output - (ones);
+			System.out.print((int) ones + " ones, ");
+		}
+		if(output / .25 >=1) {
+			quarters = (int) (output / .25);
+			output = output - (quarters * .25);
+			System.out.print((int) quarters + " quarters, ");
+		}
+		if(output / .10 >=1) {
+			dimes = (int) (output / .10);
+			output = output - (dimes * .10);
+			System.out.print((int) dimes + " dimes, ");
+		}
+		if(output / .05 >=1) {
+			nickels = (int) (output / .05);
+			output = output - (nickels * .05);
+			System.out.print((int) nickels + " nickels,");
+		}
+		if(output >= 0) {
+			pennies = (int) ((output + 0.01) * 100);
+			System.out.print(" and " + pennies + " pennies.");
+		}
+		
 		return output;
 	}
 }
